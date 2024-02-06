1eea73b8783cd405c155983ef4f4e3b4509d65a6
saleemjavid36 <saleemjavid360@gmail.com>
2024-02-06 18:39:33 +0530

    MiddlewareandCustomeMiddlewares

---
+++ Middleware/CustomMiddleware.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Middleware
+{
+    public class CustomMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            context.Response.WriteAsync("welcome to custome middle ware");
+        }
+    }
+}
